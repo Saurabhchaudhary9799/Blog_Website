c5693684d3680a678dae6e991aaf57d319620698
SaurabhChaudhary <Saurbhbailara@gmail.com>
2023-04-13 01:40:39 +0530

    fourth

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-let element = document.getElementsByClassName("item1");
-let element1 = document.getElementsByClassName("card");
-let element2 = document.querySelector(".fort1");
-console.log(element2);
-// let card = document.querySelectorAll(".card");
-// const fort = document.querySelectorAll(".fort1-info");
-// console.log(element);
-// console.log(card);
-// console.log(fort);
-console.log(element1);
-element1[0].addEventListener("click", myFunction);
-function myFunction(){
-    fort.classList.remove('hidden');
-}
+const fort1 = document.querySelector('.fort1');
+const card = document.querySelector('.fort1-info');
+const img1 = document.querySelector('.card');
+console.log(fort1);
+console.log(card);
+img1.addEventListener('mouseover', function(){
+    console.log('hi');
+    card.classList.toggle('hidden');
+})
+img1.addEventListener('mouseout', function(){
+    console.log('hi');
+    card.classList.toggle('hidden');
+})
